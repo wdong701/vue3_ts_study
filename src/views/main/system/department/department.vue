aecf0a46688eff2aadb008eaf93dc2b556142649
dlwan1 <dlwan1@amarsoft.com>
2024-04-17 11:10:08 +0800

    feat:部门管理页

--- src/views/main/system/department/department.vue
+++ src/views/main/system/department/department.vue
@@ -1,10 +1,55 @@
 <template>
   <div class="department">
-    <h2>department</h2>
+    <page-search
+      :search-config="searchConfig"
+      @query-click="handleQueryClick"
+      @reset-click="handleResetClick"
+    />
+    <page-content
+      :content-config="contentConfig"
+      ref="contentRef"
+      @new-click="handleNewClick"
+      @edit-click="handleEditClick"
+    />
+    <page-modal :modal-config="modalConfigRef" ref="modalRef" />
   </div>
 </template>
 
-<script setup lang="ts" name="department"></script>
+<script setup lang="ts">
+import PageSearch from '@/components/page-search/page-search.vue'
+import searchConfig from './config/search.config'
+
+import PageContent from '@/components/page-content/page-content.vue'
+import contentConfig from './config/content.config'
+
+import PageModal from '@/components/page-modal/page-modal.vue'
+import modalConfig from './config/modal.config'
+
+import usePageContent from '@/hooks/usePageContent'
+import usePageModal from '@/hooks/usePageModal'
+
+import useMainStore from '@/store/main/main'
+import { computed } from 'vue'
+
+const { contentRef, handleQueryClick, handleResetClick } = usePageContent()
+const { modalRef, handleNewClick, handleEditClick } = usePageModal()
+
+const modalConfigRef = computed(() => {
+  const mainStore = useMainStore()
+  const departments = mainStore.entireDepartments.map((item) => {
+    return {
+      label: item.name,
+      value: item.id
+    }
+  })
+  modalConfig.formItems.forEach((element) => {
+    if (element.prop === 'parentId') {
+      element.options.push(...departments)
+    }
+  })
+  return modalConfig
+})
+</script>
 
 <style scoped>
 .department {
